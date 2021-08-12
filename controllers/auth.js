dcc5a1500b3d549e218fd42e7eb992b257158df1
Brady Dawley <dawleyb11@gmail.com>
2021-08-12 13:43:38 -0500

    signup and the checking logic functioning, started login logic redo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const users = require('../data/userData')
 
 const fs = require('fs')
-const data = JSON.parse(fs.readFileSync('./data/users.json'));
+
 
 
 const rendLogin = (req, res, next) => {
@@ -11,24 +11,19 @@ const rendLogin = (req, res, next) => {
     })
 }
 const rendSignup = (req, res, next) => {
-    res.send('', {
-        error: false
+    res.render('signup.ejs', {
+        authMsg: false
     })
 }
 
 const login = (req, res, next) => {
-    //used when using data.json
-    let usernameFound = false;
-    for (const [id, user] of Object.entries(data)) {
-        if (user.username === req.body.username) {
-            usernameFound = id;
-        }
-    }
+    const data = JSON.parse(fs.readFileSync('./data/users.json'));
+
     //used for when using userData.js
-    // const usernameFound = users.findIndex(user => {
-    //     return user.username === req.body.username;
-    // })
-    if(usernameFound) {
+    const usernameFound = data.users.findIndex(user => {
+        return user.username === req.body.username;
+    })
+    if(usernameFound !== -1) {
         if(users[usernameFound].password === req.body.password) {
             res.redirect('/')
         }
@@ -46,8 +41,61 @@ const login = (req, res, next) => {
     }
 }
 
+const signup = (req,res,next) => {
+    try {
+        let data = JSON.parse(fs.readFileSync('./data/users.json'))
+
+        //gets input data in users.json format
+        const newUser = req.body;
+        newUser.id = String(data.users.length);
+        let check = {nameCheck: true, emailCheck: true, userCheck: true}
+        let nameCheck = true
+        let emailCheck = true
+        let userCheck = true
+
+        for(i=0;i<data.users.length; i++) {
+            if(newUser.fName === data.users[i].fName && newUser.lName === data.users[i].lName) {
+                check.nameCheck = false;
+            }      
+            if(newUser.email == data.users[i].email) {
+                check.emailCheck = false;
+            }
+            if(newUser.username === data.users[i].username) {
+                check.userCheck = false
+            }
+        }
+
+        if(check.nameCheck && check.emailCheck) {
+            if(check.userCheck) {
+                //adds data to users.json
+                data.users.push(newUser)
+                data = JSON.stringify(data, null, 5)
+
+                fs.writeFile('./data/users.json', data, (data) => {
+                    res.redirect('/')
+                }) 
+            } else {
+                res.render('signup.ejs', {
+                    auth: false,
+                    authMsg: 'Username is taken'
+                })
+            }
+        } else {
+            res.render('signup.ejs', {
+                auth: false,
+                authMsg: "You've already created an account, please login"
+            })
+        }
+        
+         
+    } catch(error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
     rendLogin,
     rendSignup, 
-    login
+    login,
+    signup
 }
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,6 @@ const authRouter = require('../controllers/auth');
 router.get('/login', authRouter.rendLogin)
 router.post('/login', authRouter.login)
 router.get('/signup', authRouter.rendSignup)
-
+router.post('/signup', authRouter.signup)
 
 module.exports = router;
